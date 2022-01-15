12c038706dcd4001a621c870ea40e0f34046bc97
Varzil <varzilthakkarvvv@gmail.com>
2022-01-15 15:58:07 +0530

    Just some addition

--- src/com/company/Adv_Pat1.java
+++ src/com/company/Adv_Pat1.java
@@ -18,32 +18,26 @@ public class Adv_Pat1 {
         System.out.println("BUTTERFLY PATTERN");
         int i,n=5;
         for(i=1;i<=n;i++){
-            for(int j=1;j<=i;j++){
-                System.out.print("*");
-            }
-            int spaces= 2*(n-i);
-            for(int j=1;j<=spaces;j++){
-                System.out.print(" ");
-            }
-            for(int j=1;j<=i;j++){
-                System.out.print("*");
-            }
-            System.out.println();
+            Dup_For(i, n);
         }
 
 
         for(i=n;i>=1;i--){
-            for(int j=1;j<=i;j++){
-                System.out.print("*");
-            }
-            int spaces= 2*(n-i);
-            for(int j=1;j<=spaces;j++){
-                System.out.print(" ");
-            }
-            for(int j=1;j<=i;j++){
-                System.out.print("*");
-            }
-            System.out.println();
+            Dup_For(i, n);
         }
-    }  
+    }
+
+    private static void Dup_For(int i, int n) {
+        for(int j=1;j<=i;j++){
+            System.out.print("*");
+        }
+        int spaces= 2*(n-i);
+        for(int j=1;j<=spaces;j++){
+            System.out.print(" ");
+        }
+        for(int j=1;j<=i;j++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
 }
